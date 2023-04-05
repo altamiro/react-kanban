9bce8b17924fba9d26d463bdcd9a5ed89f33bb7c
Altamiro Rodrigues <altamiro27@gmail.com>
2023-04-04 22:42:22 -0300

    Codigo final do curso

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './styles.css';
+import "./styles.css";
 
-import { useState } from'react';
+import { useState } from "react";
 
-import Navbar from './components/Navbar/Navbar';
-import TaskList from './components/TaskList/TaskList';
+import Navbar from "./components/Navbar/Navbar";
+import TaskList from "./components/TaskList/TaskList";
 
 /* const task = {
   id: 0,
@@ -11,32 +11,72 @@ import TaskList from './components/TaskList/TaskList';
   state: "pendente"
 } */
 
-let idAcc = 0
+let idAcc = 0;
 const generateId = () => {
-  idAcc += 1
-  return idAcc
-}
+  idAcc += 1;
+  return idAcc;
+};
 
 export default function App() {
-
   const [tasks, setTasks] = useState([]);
 
   const addTask = (title, state) => {
     const newTask = {
       id: generateId(),
       title,
-      state
+      state,
     };
     setTasks((existingTasks) => {
       return [...existingTasks, newTask];
     });
   };
 
+  const onTaskUpdate = (id, title, state) => {
+    setTasks((existingTasks) => {
+      return existingTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, title, state };
+        } else {
+          return task;
+        }
+      });
+    });
+  };
+
+  const onDeleteTask = (id) => {
+    setTasks((existingTasks) => {
+      return existingTasks.filter((task) => task.id !== id);
+    });
+  };
+
   return (
     <div className="App">
       <Navbar />
-      <div className='container'>
-        <TaskList title="Pendente" onAddTask={addTask} tasks={tasks} />
+      <div className="container">
+        <TaskList
+          title="Pendente"
+          taskState="Pendente"
+          onAddTask={addTask}
+          tasks={tasks.filter((t) => t.state === "Pendente")}
+          onTaskUpdate={onTaskUpdate}
+          onDeleteTask={onDeleteTask}
+        />
+        <TaskList
+          title="Fazendo"
+          taskState="Fazendo"
+          onAddTask={addTask}
+          tasks={tasks.filter((t) => t.state === "Fazendo")}
+          onTaskUpdate={onTaskUpdate}
+          onDeleteTask={onDeleteTask}
+        />
+        <TaskList
+          title="Completa"
+          taskState="Completa"
+          onAddTask={addTask}
+          tasks={tasks.filter((t) => t.state === "Completa")}
+          onTaskUpdate={onTaskUpdate}
+          onDeleteTask={onDeleteTask}
+        />
       </div>
     </div>
   );
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -2,10 +2,20 @@ import "./tasklist.css";
 
 import PropTypes from "prop-types";
 
-export default function TaskList({ title, onAddTask, tasks}) {
+import TaskItem from "../TaskItem/TaskItem";
 
+import plusIcon from "../../img/plus-icon.svg";
+
+export default function TaskList({
+  title,
+  taskState,
+  onAddTask,
+  tasks,
+  onTaskUpdate,
+  onDeleteTask,
+}) {
   const addTask = () => {
-    onAddTask("Nova Tarefa", "Pendente");
+    onAddTask("Nova Tarefa", taskState);
   };
 
   return (
@@ -13,16 +23,32 @@ export default function TaskList({ title, onAddTask, tasks}) {
       <div className="title">{title}</div>
       <div className="content">
         {tasks.map((task) => {
-           return <div key={task.id}>{task.title}</div>
+          return (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              taskState={task.state}
+              onTaskUpdate={onTaskUpdate}
+              onDeleteTask={onDeleteTask}
+            />
+          );
         })}
+        {tasks.length === 0 && <div className="empty-list">Lista Vazia</div>}
+        <button onClick={addTask} className="btn">
+          <img src={plusIcon} alt="plus" />
+          Adicionar Tarefa
+        </button>
       </div>
-      <button onClick={addTask}>Adicionar Tarefa</button>
     </div>
   );
 }
 
 TaskList.propTypes = {
   title: PropTypes.string.isRequired,
+  taskState: PropTypes.string.isRequired,
   onAddTask: PropTypes.func.isRequired,
-  tasks: PropTypes.array.isRequired
+  tasks: PropTypes.array.isRequired,
+  onTaskUpdate: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired,
 };
